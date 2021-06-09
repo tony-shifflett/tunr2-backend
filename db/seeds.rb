f5c1879774a7fe628fcaff8c05ed51875d0ca711
Tony Shifflett <charles.l.shifflett@gmail.com>
2021-06-09 11:06:37 -0500

    completed initial setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Song.create(title: "Electric Lady", artist: "Janelle Monae" , time: "5:08", favorite: false)
+Song.create(title: "Road to Nowhere", artist: "Talking Heads", time: "3:59" , favorite: false)
+Song.create(title: "Do You" , artist: "Spoon", time: "3:34" , favorite: "false")
